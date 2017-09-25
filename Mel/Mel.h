63e15c66b4c50e878b6b0f47113fe8a6d1e837db
wuillaumea <wuillaumea@gmail.com>
2017-09-25 15:04:36 -0400

    Add project files.

---
+++ Mel/Mel.h
@@ -0,0 +1,35 @@
+
+// Mel.h : main header file for the Mel application
+//
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "include 'stdafx.h' before including this file for PCH"
+#endif
+
+#include "resource.h"       // main symbols
+
+
+// CMelApp:
+// See Mel.cpp for the implementation of this class
+//
+
+class CMelApp : public CWinApp
+{
+public:
+	CMelApp();
+
+
+// Overrides
+public:
+	virtual BOOL InitInstance();
+	virtual int ExitInstance();
+
+// Implementation
+
+public:
+	afx_msg void OnAppAbout();
+	DECLARE_MESSAGE_MAP()
+};
+
+extern CMelApp theApp;
