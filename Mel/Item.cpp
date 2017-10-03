e9fbbea0f45da52cdf379c86804302366bcf99d3
Aaron <aaronje13@gmail.com>
2017-10-03 00:04:39 -0400

    Added CItem class, made it circularly dependent with CGame class.

--- Mel/Item.cpp
+++ Mel/Item.cpp
@@ -1,12 +1,94 @@
+/**
+* \file Item.cpp
+*
+* \author Aaron Eshleman
+*/
+
 #include "stdafx.h"
 #include "Item.h"
+#include "Game.h"
+#include <string>
+
+using namespace Gdiplus;
+using namespace std;
 
 
-CItem::CItem()
+
+void CItem::SetLocation(double x, double y)
+{
+	mX = x;
+	mY = y;
+}
+
+/** Constructor
+* \param game The game this item is a member of
+*/
+CItem::CItem(CGame *game) : mGame(game)
 {
 }
 
 
+/**
+* Destructor
+*/
 CItem::~CItem()
 {
 }
+
+/**
+*  Test to see if a coordinate postion is within a certain item image.
+* \param x X position to test
+* \param y Y position to test
+* \return true if hit.
+*/
+bool CItem::HitTest(int x, int y)
+{
+	double wid = mItemImage->GetWidth();
+	double hit = mItemImage->GetHeight();
+
+	// Make x and y relative to the top-left corner of the bitmap image.
+	// Subtracting the center makes x, y relative to the center of 
+	// the image. Adding half the size makes x, y relative to the top 
+	// corner of the image.
+	double testX = x - GetX() + wid / 2;
+	double testY = y - GetY() + hit / 2;
+
+	// Test to see if x, y are in the image
+	if (testX < 0 || testY < 0 || testX >= wid || testY >= hit)
+	{
+		// We are outside the image
+		return false;
+	}
+
+	// Test to see if x, y are in the drawn part of the image
+	auto format = mItemImage->GetPixelFormat();
+	if (format == PixelFormat32bppARGB || format == PixelFormat32bppPARGB)
+	{
+		// This image has an alpha map, which implements the 
+		// transparency. If so, we should check to see if we
+		// clicked on a pixel where alpha is not zero, meaning
+		// the pixel shows on the screen.
+		Color color;
+		mItemImage->GetPixel((int)testX, (int)testY, &color);
+		return color.GetAlpha() != 0;
+	}
+	else {
+		return true;
+	}
+}
+
+/**
+* Draw this item
+* \param graphics Graphics device to draw on
+*/
+void CItem::Draw(Gdiplus::Graphics *graphics)
+{
+	double wid = mItemImage->GetWidth();
+	double hit = mItemImage->GetHeight();
+
+	graphics->DrawImage(mItemImage.get(),
+		float(GetX() - wid / 2), float(GetY() - hit / 2),
+		(float)mItemImage->GetWidth(), (float)mItemImage->GetHeight());
+	
+}
+
--- Mel/Item.h
+++ Mel/Item.h
@@ -1,8 +1,87 @@
+/**
+* \file Item.h
+*
+* \author Aaron Eshleman
+*
+* Class that holds location information about each item in the game. (Gru, Minions, Villians)
+*
+* Circularly dependent on CGame
+*
+*/
+
 #pragma once
+#include <memory>
+#include <string>
+
+class CGame;
+
+/**
+* Data about items in game.
+*/
 class CItem
 {
 public:
-	CItem();
 	virtual ~CItem();
+
+	/// Default constructor (disabled)
+	CItem() = delete;
+
+	/// Copy constructor (disabled)
+	CItem(const CItem &) = delete;
+
+	/** The X location of the item
+	* \returns X location in pixels */
+	double GetX() const { return mX; }
+
+	/** The Y location of the item
+	* \returns Y location in pixels */
+	double GetY() const { return mY; }
+
+	/// Set the item location
+	/// \param x X location
+	/// \param y Y location
+	virtual void SetLocation(double x, double y);
+
+	/// Draw this item
+	/// \param graphics Graphics device to draw on
+	virtual void Draw(Gdiplus::Graphics *graphics);
+
+	/** Test this item to see if it has been clicked on
+	* \param x X location on the game to test
+	* \param y Y location on the game to test
+	* \return true if clicked on */
+	virtual bool HitTest(int x, int y);
+
+	/// Handle updates for animation
+	/// \param elapsed The time since the last update
+	virtual void Update(double elapsed) {}
+
+	/// Get the game this item is in
+	/// \returns Game pointer
+	CGame *GetGame() { return mGame; }
+
+	///Get the item image width for particular item
+	double GetItemImageWidth() { return mItemImage->GetWidth(); }
+
+	///Get the image height for particular item
+	double GetItemImageHeight() { return mItemImage->GetHeight(); }
+
+
+protected:
+	///Constructs
+	CItem(CGame *game);
+
+
+private:
+	/// The game this item is contained in
+	CGame   *mGame;
+
+	// Item location in the game
+	double  mX = 0;     ///< X location for the center of the item
+	double  mY = 0;     ///< Y location for the center of the item
+
+	/// The image of this item.
+	std::unique_ptr<Gdiplus::Bitmap> mItemImage;
+
 };
 
