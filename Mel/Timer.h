9a5841aeb700a4bbabecf4a8ec52810424071d17
Tyler Gasper <tyler@flatrivergroup.com>
2017-10-03 20:58:10 -0400

    made the inital timer class

---
+++ Mel/Timer.h
@@ -0,0 +1,24 @@
+/**
+* \file Timer.h
+*
+* \author Tyler Gasper
+*
+* Class that will implement the game timer
+*/
+
+
+#pragma once
+class CTimer
+{
+public:
+	CTimer();
+	~CTimer();
+
+	void CTimer::OnDraw(Gdiplus::Graphics * graphics);
+
+private:
+
+	long mStartTime; ///< Time that the game was started 
+
+};
+
