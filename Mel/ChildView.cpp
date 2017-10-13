bb2a91c0801c8c824f1314153acc00ed0e568eae
Aaron <aaronje13@gmail.com>
2017-10-13 13:17:53 -0400

    Calls reset game from L Button Down

--- Mel/ChildView.cpp
+++ Mel/ChildView.cpp
@@ -174,8 +174,14 @@ void CChildView::OnLButtonDown(UINT nFlags, CPoint point)
 
 	mGrabbedItem = mGame.HitTest(point.x, point.y);
 	if (mGrabbedItem->IsMovable() != true)
-	{	
+	{
+		if (mGrabbedItem->IsNewGame())
+		{
+			mGame.ResetGame();
+			mGrabbedItem = nullptr;
+		}
 		mGrabbedItem = nullptr;
+
 	}
 }
 
