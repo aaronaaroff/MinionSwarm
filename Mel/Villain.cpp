8df6c9b0fadc55a8563b9d52e154ae8794cb7a32
Alex Wuillaume <wuillaumea@gmail.com>
2017-10-20 21:19:41 -0400

    docs

--- Mel/Villain.cpp
+++ Mel/Villain.cpp
@@ -23,9 +23,14 @@ const wstring PokeBallImageName = L"images/pokeball.png";
 /// Blender Villain filename 
 const wstring BlenderImageName = L"images/juicer.png";
 
+/// arya mult
+const double ARYA_M = 1;
 
+/// blend mult
+const double BLENDER_M = 2;
 
-
+/// poke mult
+const double POKE_M = 3;
 
 CVillain::~CVillain()
 {
@@ -37,18 +42,18 @@ CVillain::CVillain(CGame * game, std::wstring name) :
 	if (name == L"Arya")
 	{
 		mVillainType = AryaStark;
-		mPointMultiplier = 1;
+		mPointMultiplier = ARYA_M;
 	}
 	else if (name == L"Blender")
 	{
 		mVillainType = Blender;
-		mPointMultiplier = 2;
+		mPointMultiplier = BLENDER_M;
 		setImage(BlenderImageName);
 	}
 	else
 	{
 		mVillainType = PokeBall;
-		mPointMultiplier = 3;
+		mPointMultiplier = POKE_M;
 		setImage(PokeBallImageName);
 
 	}
